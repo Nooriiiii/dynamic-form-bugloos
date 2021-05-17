3588d649ca81428cd20a9fda71ac49bf5e772c1a
Nooriiiii <elham.j.noori@gmail.com>
2021-05-17 10:23:43 +0430

    ui changes

--- src/app/dynamic-form/dynamic-form.service.ts
+++ src/app/dynamic-form/dynamic-form.service.ts
@@ -1,4 +1,4 @@
-import { DropdownField, TextboxField, BaseField, EmailField } from './fields';
+import { DropdownField, TextboxField, BaseField, EmailField, NumberField } from './fields';
 import { Injectable } from "@angular/core";
 
 @Injectable({providedIn: 'root'})
@@ -7,32 +7,38 @@ export class DynamicFormService{
   getForm() : BaseField[]{
     return [
 
-      new DropdownField({
-        key: 'brave',
-        label: 'Bravery Rating',
-        options: [
-          {key: 'solid',  value: 'Solid'},
-          {key: 'great',  value: 'Great'},
-          {key: 'good',   value: 'Good'},
-          {key: 'unproven', value: 'Unproven'}
-        ],
-        order: 3
+      new TextboxField({
+        key: 'name',
+        label: 'First Name',
+        required:true,
+        order: 1
       }),
 
-      new TextboxField({
-        key: 'firstName',
-        label: 'First name',
-        value: 'Bombasto',
-        required: true,
-        order: 1
+      new NumberField({
+        key: 'age',
+        label: 'Age',
+        value: '30',
+        type:'number',
+        order: 2
       }),
 
       new EmailField({
         key: 'emailAddress',
         label: 'Email',
         type: 'email',
-        order: 2
-      })
+        order: 3
+      }),
+
+      new DropdownField({
+        key: 'role',
+        label: 'Role',
+        options: [
+          {key: 'admin',  value: 'Admin'},
+          {key: 'owner',  value: 'Owner'},
+          {key: 'user',   value: 'User'}
+        ],
+        order: 4
+      }),
     ]
   }
 
--- src/app/dynamic-form/fields/NumberField.ts
+++ src/app/dynamic-form/fields/NumberField.ts
@@ -2,5 +2,5 @@ import { BaseField } from './BaseField';
 
 export class NumberField extends BaseField{
   controlType = 'textbox';
-  type = "number"
+  type = "Number"
 }
